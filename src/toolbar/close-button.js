afa2ac0877cb71a06f1d0bde77bf368a7beea669
Pedro Lopez <39785338+plopez9@users.noreply.github.com>
2019-10-16 13:59:02 -0500

    10/16/19
    working side nav (background x needs to be changed)

--- src/toolbar/close-button.js
+++ src/toolbar/close-button.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-
+import "./toolbar-css/close-button.css";
 
 const CloseButton = props => (
-  <button className="button" onClick={props.click}>
-    <div className="marker">X</div>
+  <button className="closeButton" onClick={props.click}>
+    <div className="closeMarker">X</div>
   </button>
 )
 
